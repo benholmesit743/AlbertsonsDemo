4234b1b778af463497d9de1475a7e336985a37fb
Ben Holmes <ben.holmes@aa.com>
2022-07-23 16:16:41 -0400

    support reconfiguration changes

--- app/src/main/java/com/example/AlbertsonsDemo/MainActivity.kt
+++ app/src/main/java/com/example/AlbertsonsDemo/MainActivity.kt
@@ -10,17 +10,16 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -48,12 +47,12 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainScreen(viewModel: MainViewModel) {
-    val textState = remember { mutableStateOf(TextFieldValue("")) }
-    val textString = textState.value.text
-    if (!textString.isEmpty()) viewModel.searchText(textString)
+    var searchText by rememberSaveable { mutableStateOf("") }
+
+    if (!searchText.isEmpty()) viewModel.searchText(searchText)
 
     Column {
-        SearchView(textState)
+        SearchView(searchText, onSearchTextChanged = { searchText = it })
         SearchList(viewModel)
     }
 }
@@ -65,12 +64,10 @@ fun MainScreenPreview() {
 }
 
 @Composable
-fun SearchView(state: MutableState<TextFieldValue>) {
+fun SearchView(searchText: String, onSearchTextChanged: (String) -> Unit) {
     TextField(
-        value = state.value,
-        onValueChange = { value ->
-            state.value = value
-        },
+        value = searchText,
+        onValueChange = onSearchTextChanged,
         modifier = Modifier
             .fillMaxWidth(),
         textStyle = TextStyle(color = Color.White, fontSize = 18.sp),
@@ -82,24 +79,6 @@ fun SearchView(state: MutableState<TextFieldValue>) {
                     .padding(15.dp)
                     .size(24.dp)
             )
-        },
-        trailingIcon = {
-            if (state.value != TextFieldValue("")) {
-                IconButton(
-                    onClick = {
-                        state.value =
-                            TextFieldValue("") // Remove text from TextField when you press the 'X' icon
-                    }
-                ) {
-                    Icon(
-                        Icons.Default.Close,
-                        contentDescription = "",
-                        modifier = Modifier
-                            .padding(15.dp)
-                            .size(24.dp)
-                    )
-                }
-            }
         },
         singleLine = true,
         shape = RectangleShape, // The TextFiled has rounded corners top left and right by default
@@ -119,8 +98,7 @@ fun SearchView(state: MutableState<TextFieldValue>) {
 @Preview(showBackground = true)
 @Composable
 fun SearchViewPreview() {
-    val textState = remember { mutableStateOf(TextFieldValue("")) }
-    SearchView(textState)
+    SearchView("", onSearchTextChanged = { var searchText = it })
 }
 
 @Composable
